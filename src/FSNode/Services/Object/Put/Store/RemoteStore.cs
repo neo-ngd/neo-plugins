3b3c3663b4ed39e4c875bfc06e64ef188a9ce22c
ZhangTao1596 <zhangtao@ngd.neo.org>
2020-12-24 18:16:48 +0800

    update object remote sender

--- src/FSNode/Services/Object/Put/Store/RemoteStore.cs
+++ src/FSNode/Services/Object/Put/Store/RemoteStore.cs
@@ -1,13 +1,37 @@
-using NeoFS.API.v2.Refs;
+using NeoFS.API.v2.Acl;
+using NeoFS.API.v2.Session;
 using V2Object = NeoFS.API.v2.Object.Object;
+using Neo.FSNode.Network.Cache;
+using Neo.FSNode.Services.Object.Util;
+using System;
 
 namespace Neo.FSNode.Services.Object.Put.Store
 {
     public class RemoteStore : IStore
     {
+        private readonly KeyStorage keyStorage;
+        private readonly ClientCache clientCache;
+        private readonly Network.Address node;
+        private readonly SessionToken sessionToken;
+        private readonly BearerToken bearerToken;
+
         public void Put(V2Object obj)
         {
-
+            var key = keyStorage.GetKey(sessionToken);
+            if (key is null)
+                throw new InvalidOperationException(nameof(Range) + " could not receive private key");
+            var addr = node.IPAddressString();
+            var client = clientCache.GetClient(key, addr);
+            if (client is null)
+                throw new InvalidOperationException(nameof(Range) + $" could not create SDK client {addr}");
+            var oid = client.PutObject(obj, new NeoFS.API.v2.Client.CallOptions
+            {
+                Ttl = 1,
+                Session = sessionToken,
+                Bearer = bearerToken,
+            }).Result;
+            if (oid is null)
+                throw new InvalidOperationException(nameof(Range) + $" could not read object payload range from {addr}");
         }
     }
 }
