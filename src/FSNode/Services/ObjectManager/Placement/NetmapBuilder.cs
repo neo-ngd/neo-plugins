a7cd02d2f021ed5df8394bb9416241fbc7a1b623
ZhangTao1596 <zhangtao@ngd.neo.org>
2020-12-29 15:40:18 +0800

    finish NetmapBuilder

--- src/FSNode/Services/ObjectManager/Placement/NetmapBuilder.cs
+++ src/FSNode/Services/ObjectManager/Placement/NetmapBuilder.cs
@@ -1,6 +1,7 @@
 using Neo.FSNode.Core.Netmap;
 using NeoFS.API.v2.Netmap;
 using NeoFS.API.v2.Refs;
+using System;
 using System.Collections.Generic;
 
 namespace Neo.FSNode.Services.ObjectManager.Placement
@@ -19,15 +20,27 @@ namespace Neo.FSNode.Services.ObjectManager.Placement
             netmapSource = new NetmapSource(netMap);
         }
 
-        public virtual List<Node[]> BuildPlacement(Address address, PlacementPolicy pp)
+        public virtual List<Node[]> BuildPlacement(Address address, PlacementPolicy policy)
         {
-            return new List<Node[]>();
+            var netmap = netmapSource.GetLatestNetworkMap();
+            var nodes = netmap.GetContainerNodes(policy, address.ContainerId.Value.ToByteArray());
+            return BuildObjectPlacement(netmap, nodes, address.ObjectId);
+        }
+
+        public static List<Node[]> BuildObjectPlacement(NetMap netmap, List<Node[]> container_nodes, ObjectID oid)
+        {
+            if (oid is null)
+                return container_nodes;
+            var ns = netmap.GetPlacementVectors(container_nodes, oid.Value.ToByteArray());
+            if (ns is null)
+                throw new InvalidOperationException(nameof(BuildObjectPlacement) + " could not get placement vectors for object");
+            return ns;
         }
     }
 
     public class NetmapSource : INetmapSource
     {
-        private NetMap netmap;
+        private readonly NetMap netmap;
 
         public NetmapSource(NetMap netmap)
         {
